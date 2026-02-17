730929841a81a3df48f0aa7ea2eddfa452b8dce3
tlsstern <tl@stern.ch>
2026-02-17 13:53:59 +0100

    Clond everything

---
+++ KN02/db.php
@@ -0,0 +1,21 @@
+<?php
+        //database
+        $servername = "servername";
+        $username = "username";
+        $password = "password";
+        $dbname = "dbname";
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+        }
+
+        $sql = "select Host, User from mysql.user;";
+        $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()){
+                echo($row["Host"] . " / " . $row["User"] . "<br />");
+        }
+        //var_dump($result);
+?>
